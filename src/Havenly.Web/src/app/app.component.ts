b4f8bdee2b6b46ac0cb4bb2ae53a818b3a2934d6
hmrguez <zealot.algo@gmail.com>
2024-04-12 08:53:08 -0400

    Refactored frontend project

--- src/Havenly.Web/src/app/app.component.ts
+++ src/Havenly.Web/src/app/app.component.ts
@@ -1,12 +1,7 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterOutlet} from '@angular/router';
-import {LoginComponent} from "./login/login.component";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
