1a3fcdb5ac846f557118aab1219d7ebd0ac614a4
hmrguez <zealot.algo@gmail.com>
2024-04-13 11:19:36 -0400

    Added routing

--- src/Havenly.Web/src/app/app-routing.module.ts
+++ src/Havenly.Web/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {LoginComponent} from "./auth/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/Havenly.Web/src/app/app.module.ts
+++ src/Havenly.Web/src/app/app.module.ts
@@ -7,11 +7,15 @@ import { LoginComponent } from './auth/login/login.component';
 import {FormsModule} from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
 import { GraphQLModule } from './graphql.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
